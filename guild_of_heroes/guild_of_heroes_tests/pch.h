945852474e766719af6702902dcf6d752d77e275
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-12-21 15:00:08 +0300

    add #define EXPECT_THROW_WITH_MESSAGE

--- guild_of_heroes/guild_of_heroes_tests/pch.h
+++ guild_of_heroes/guild_of_heroes_tests/pch.h
@@ -4,4 +4,20 @@
 
 #pragma once
 
+#include <string>
+#include <algorithm>
+#include <stdexcept>
 #include "gtest/gtest.h"
+
+#define EXPECT_THROW_WITH_MESSAGE(statement, exception, message) \
+	try {\
+		(statement);\
+		FAIL() << "Error: expect " << #exception; \
+	} catch (const exception& ex) {\
+		std::string exp_msg(message);\
+		std::transform(exp_msg.begin(), exp_msg.end(), exp_msg.begin(), \
+					  [](unsigned char c){ return std::tolower(c); });\
+		EXPECT_TRUE(std::string(ex.what()).find(exp_msg) != std::string::npos);\
+	} catch (...) {\
+		FAIL() << "Error: expect " << #exception; \
+	}
